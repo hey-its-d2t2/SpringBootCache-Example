d02dc399a4de45da44dbc3d7cae58e56c5d5d2e0
Deepak Singh <deepsinghkumar01@gmail.com>
2025-01-09 22:28:52 +0530

    Updated: Find by age method added.

--- src/main/java/com/exCache/repo/UserRepository.java
+++ src/main/java/com/exCache/repo/UserRepository.java
@@ -3,5 +3,10 @@ package com.exCache.repo;
 import com.exCache.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<Object> findByAge(int age);
 }
