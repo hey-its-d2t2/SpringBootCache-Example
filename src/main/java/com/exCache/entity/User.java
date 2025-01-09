465ec81aa851ba2360247b9211ada9edf00a37f2
Deepak Singh <deepsinghkumar01@gmail.com>
2025-01-09 22:27:56 +0530

    Updated: Getters & Setters added.

--- src/main/java/com/exCache/entity/User.java
+++ src/main/java/com/exCache/entity/User.java
@@ -18,11 +18,49 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String name;
-    private String age;
+    private int age;
     private String gender;
     private String email;
-
-
 
 }
